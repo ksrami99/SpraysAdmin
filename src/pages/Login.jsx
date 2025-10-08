36427d2229dd30e558450a7163b4c3b3b3de38bf
Rami Karan <99781533+ksrami99@users.noreply.github.com>
2025-10-08 16:00:37 +0530

    Refactor Login component to use MUI components and improve styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,38 +2,6 @@ import { useState } from "react";
 import { loginService } from "../services/auth.service";
 import { useNavigate } from "react-router-dom";
 
-// export default function Login() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleLogin = async (e) => {
-//     e.preventDefault();
-//     const res = await loginService(email, password);
-//     if (res.success) navigate("/admin");
-//   };
-
-//   return (
-//     <div>
-//       <h2>Login</h2>
-//       <form onSubmit={handleLogin}>
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <button type="submit">Login</button>
-//       </form>
-//     </div>
-//   );
-// }
 import * as React from "react";
 import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import Sheet from "@mui/joy/Sheet";
